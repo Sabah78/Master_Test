a549ba297b695294ae796c997d0335dc8f7108bc
hp <hp@pavilion>
2020-04-24 14:12:16 +0530

    Test1 done

---
+++ Object Repository/Test/td_email.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>td_email</name>
+   <tag></tag>
+   <elementGuidId>af81f2c2-17cf-4a2d-896e-cf18a3e99b99</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class=&quot;menu_login_container rfloat _ohf&quot;]//form//table//tbody//tr[2]//td[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class=&quot;menu_login_container rfloat _ohf&quot;]//form//table//tbody//tr[2]//td[1]</value>
+   </webElementProperties>
+</WebElementEntity>
